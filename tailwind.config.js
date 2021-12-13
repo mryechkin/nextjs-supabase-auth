6db1dc2ef3c6eae3b459d99e3cb0d35e19854a25
Mykhaylo Ryechkin <5657181+mryechkin@users.noreply.github.com>
2021-12-12 23:00:08 -0500

    chore: Updating to Tailwind 3.0 🚀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
