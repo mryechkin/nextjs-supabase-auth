d21bf10aa5c0d445b63e1d97669b7600c0ef91c3
Amad Ansari <amadansari@amad-m1.local>
2023-06-07 10:46:20 -0400

    disable linting for now

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   experimental: {
     appDir: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
